f4eb8eab9d10572db1ee2263716bc13813d643e0
Fellipe Utaka <fellipeutaka@gmail.com>
2023-10-28 17:37:37 -0300

    feat(ui): export dialog styles

--- packages/ui/src/dialog.tsx
+++ packages/ui/src/dialog.tsx
@@ -2,9 +2,49 @@
 
 import { forwardRef } from "react";
 
-import { cn } from "@fellipeutaka/styles";
+import { tv } from "@fellipeutaka/styles";
 import * as DialogPrimitive from "@radix-ui/react-dialog";
 import { X } from "lucide-react";
+
+export const DialogStyles = {
+  Overlay: tv({
+    base: [
+      "fixed inset-0 z-50 bg-background/80 backdrop-blur-sm",
+      "data-[state=open]:animate-in data-[state=open]:fade-in-0",
+      "data-[state=closed]:animate-out data-[state=closed]:fade-out-0",
+    ],
+  }),
+  Content: tv({
+    base: [
+      "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-border bg-background p-6 shadow-lg duration-200",
+      "data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%]",
+      "data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%]",
+      "sm:rounded-lg",
+      "md:w-full",
+    ],
+  }),
+  Close: tv({
+    base: [
+      "absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition",
+      "hover:opacity-100",
+      "focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2",
+      "disabled:pointer-events-none",
+      "data-[state=open]:bg-accent data-[state=open]:text-muted-foreground",
+    ],
+  }),
+  Header: tv({
+    base: ["flex flex-col space-y-1.5 text-center", "sm:text-left"],
+  }),
+  Footer: tv({
+    base: ["flex flex-col-reverse", "sm:flex-row sm:justify-end sm:space-x-2"],
+  }),
+  Title: tv({
+    base: ["text-lg font-semibold leading-none tracking-tight"],
+  }),
+  Description: tv({
+    base: ["text-sm text-muted-foreground"],
+  }),
+};
 
 const DialogRoot = DialogPrimitive.Root;
 
@@ -20,10 +60,7 @@ const DialogOverlay = forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Overlay
     ref={ref}
-    className={cn(
-      "fixed inset-0 z-50 bg-background/80 backdrop-blur-sm data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-      className,
-    )}
+    className={DialogStyles.Overlay({ className })}
     {...props}
   />
 ));
@@ -37,16 +74,13 @@ const DialogContent = forwardRef<
     <DialogOverlay />
     <DialogPrimitive.Content
       ref={ref}
-      className={cn(
-        "fixed left-[50%] top-[50%] z-50 grid w-full max-w-lg translate-x-[-50%] translate-y-[-50%] gap-4 border border-border bg-background p-6 shadow-lg duration-200 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[state=closed]:slide-out-to-left-1/2 data-[state=closed]:slide-out-to-top-[48%] data-[state=open]:slide-in-from-left-1/2 data-[state=open]:slide-in-from-top-[48%] sm:rounded-lg md:w-full",
-        className,
-      )}
+      className={DialogStyles.Content({ className })}
       {...props}
     >
       {children}
       <DialogPrimitive.Close
         aria-label="Close"
-        className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-accent data-[state=open]:text-muted-foreground"
+        className={DialogStyles.Close()}
       >
         <X className="h-4 w-4" />
       </DialogPrimitive.Close>
@@ -59,13 +93,7 @@ const DialogHeader = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      "flex flex-col space-y-1.5 text-center sm:text-left",
-      className,
-    )}
-    {...props}
-  />
+  <div className={DialogStyles.Header({ className })} {...props} />
 );
 DialogHeader.displayName = "DialogHeader";
 
@@ -73,13 +101,7 @@ const DialogFooter = ({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) => (
-  <div
-    className={cn(
-      "flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2",
-      className,
-    )}
-    {...props}
-  />
+  <div className={DialogStyles.Footer({ className })} {...props} />
 );
 DialogFooter.displayName = "DialogFooter";
 
@@ -89,10 +111,7 @@ const DialogTitle = forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Title
     ref={ref}
-    className={cn(
-      "text-lg font-semibold leading-none tracking-tight",
-      className,
-    )}
+    className={DialogStyles.Title({ className })}
     {...props}
   />
 ));
@@ -104,21 +123,11 @@ const DialogDescription = forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Description
     ref={ref}
-    className={cn("text-sm text-muted-foreground", className)}
+    className={DialogStyles.Description({ className })}
     {...props}
   />
 ));
 DialogDescription.displayName = DialogPrimitive.Description.displayName;
-
-const Dialog = {
-  Root: DialogRoot,
-  Trigger: DialogTrigger,
-  Content: DialogContent,
-  Title: DialogTitle,
-  Description: DialogDescription,
-  Header: DialogHeader,
-  Footer: DialogFooter,
-};
 
 function handleCloseDialog() {
   document.dispatchEvent(
@@ -127,6 +136,16 @@ function handleCloseDialog() {
     }),
   );
 }
+
+const Dialog = Object.assign(DialogRoot, {
+  Root: DialogRoot,
+  Trigger: DialogTrigger,
+  Content: DialogContent,
+  Title: DialogTitle,
+  Description: DialogDescription,
+  Header: DialogHeader,
+  Footer: DialogFooter,
+});
 
 export {
   Dialog,
