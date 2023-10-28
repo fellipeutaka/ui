277d26537979ee43ad715e30f4a4edaded58a6cf
Fellipe Utaka <fellipeutaka@gmail.com>
2023-10-28 16:20:40 -0300

    docs(dialog): add label and textfield component to story example

--- apps/docs/src/stories/components/dialog.stories.tsx
+++ apps/docs/src/stories/components/dialog.stories.tsx
@@ -1,5 +1,7 @@
 import { Button } from "@fellipeutaka/ui/button";
 import { Dialog } from "@fellipeutaka/ui/dialog";
+import { Label } from "@fellipeutaka/ui/label";
+import { TextField } from "@fellipeutaka/ui/textfield";
 import type { Meta, StoryObj } from "@storybook/react";
 
 type DialogProps = React.ComponentPropsWithoutRef<(typeof Dialog)["Root"]>;
@@ -20,6 +22,24 @@ const meta: Meta<DialogProps> = {
               Make changes to your profile here. Click save when you're done.
             </Dialog.Description>
           </Dialog.Header>
+          <div className="grid gap-4 py-4">
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="name" className="text-right">
+                Name
+              </Label>
+              <TextField.Root className="col-span-3">
+                <TextField.Input id="name" defaultValue="Pedro Duarte" />
+              </TextField.Root>
+            </div>
+            <div className="grid grid-cols-4 items-center gap-4">
+              <Label htmlFor="username" className="text-right">
+                Username
+              </Label>
+              <TextField.Root className="col-span-3">
+                <TextField.Input id="username" defaultValue="@peduarte" />
+              </TextField.Root>
+            </div>
+          </div>
           <Dialog.Footer>
             <Button>Save changes</Button>
           </Dialog.Footer>
